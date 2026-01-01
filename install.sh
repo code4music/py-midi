2983dc187a882cd632fdbbe489c1610a51497d32
Rafael Mateus <rafaelbmateus.dev@gmail.com>
2026-01-01 14:34:19 -0300

    chore: testing install in raspberry pi 3

--- install.sh
+++ install.sh
@@ -3,10 +3,12 @@ set -e
 
 apt update
 
-apt install -y build-essential python3-dev python3-pip \
- fluidsynth portaudio19-dev libasound2-dev python3-rtmidi
-
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-
-echo "Instalação completa. Coloque seus .sf2 em ./sounds/ e edite config.yaml"
+apt install -y \
+  build-essential \
+  make \
+  python3-dev \
+  python3-pip \
+  python3-rtmidi \
+  fluidsynth \
+  portaudio19-dev \
+  libasound2-dev
